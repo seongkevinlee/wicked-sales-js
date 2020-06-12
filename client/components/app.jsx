c0f2ee3a9787e675202f528311fbdc503d9c7c7c
seongkevinlee <seongkevinlee@gmail.com>
2020-06-12 13:25:26 -0700

    Updated App component to have view state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,17 +6,15 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      view: {
+        name: 'catalog',
+        params: {}
+      }
     };
   }
 
   componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
+
   }
 
   render() {
