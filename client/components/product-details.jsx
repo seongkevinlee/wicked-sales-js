d2b471ad8436bc9b3361592d5618c28ccc5b129e
seongkevinlee <seongkevinlee@gmail.com>
2020-06-12 16:14:21 -0700

    Added ability to render item info that is clicked and switch between views

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -14,11 +14,11 @@ export default class ProductDetails extends React.Component {
   }
 
   getProduct() {
-    fetch('api/products/1')
+    fetch(`api/products/${this.props.params}`)
       .then(response => response.json())
       .then(data => {
         this.setState({
-          products: data
+          product: data
         });
       })
       .catch(err => console.error(err));
@@ -26,8 +26,24 @@ export default class ProductDetails extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>product details</h1>
+      <div className='container mt-3'>
+        <div className='mb-4'>
+          <button className='btn btn-link' onClick={() => this.props.setView('catalog', {})}>&lt; Back to catalog</button>
+        </div>
+        <div className='d-flex no-wrap'>
+          <div className='col-6'>
+            <img className='img-fluid' src={this.state.product ? this.state.product.image : 'No product found'} alt=""/>
+          </div>
+          <div className='d-flex flex-column justify-content-start pt-2'>
+            <h1>{this.state.product ? this.state.product.name : 'No product found'}</h1>
+            <h4 className='text-muted mt-3 mb-3'>{this.state.product ? `$${this.state.product.price}` : 'No product found'}</h4>
+            <p>{this.state.product ? this.state.product.shortDescription : 'No product found'}</p>
+          </div>
+        </div>
+        <p className='mt-4'>{this.state.product ? this.state.product.longDescription : 'No product found'}</p>
+        <div>
+
+        </div>
       </div>
     );
   }
