9f930dbe4bb3c90fbe0f30422450087541443c1c
seongkevinlee <seongkevinlee@gmail.com>
2020-09-24 23:08:58 -0700

    added warning modal to checkout page

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 import CalculateTotalCost from './calculate-total-cost';
+import WarningModal from './modal';
 
 export default class CartSummary extends React.Component {
   constructor(props) {
@@ -22,25 +23,28 @@ export default class CartSummary extends React.Component {
 
   render() {
     return (
-      <div className="cart-summary">
-        <div className="m-auto col-sm-11">
-          <button className='back-btn btn btn-info'
-            onClick={() => this.props.setView('catalog', {})}>
+      <div>
+        <WarningModal/>
+        <div className="cart-summary">
+          <div className="m-auto col-sm-11">
+            <button className='back-btn btn btn-info'
+              onClick={() => this.props.setView('catalog', {})}>
             &lt; Back to catalog
-          </button>
-        </div>
-        <div className='d-flex flex-column align-items-center col-12'>
-          {this.props.cart.length <= 0 ? <h3 className='no-items text-white m-5'>There are no items in your cart</h3> : this.renderCartItems()}
-        </div>
-        <div className='col-11 m-auto d-flex justify-content-between align-items-center'>
-          <h2 className="cart-total">
-            {`Total: $${CalculateTotalCost(this.props)}.00`}
-          </h2>
-          <button
-            className={this.props.cart.length === 0 ? 'checkout-btn btn btn-primary d-none' : 'checkout-btn btn btn-primary' }
-            onClick={() => this.props.setView('checkout', {})}>
+            </button>
+          </div>
+          <div className='d-flex flex-column align-items-center col-12'>
+            {this.props.cart.length <= 0 ? <h3 className='no-items text-white m-5'>There are no items in your cart</h3> : this.renderCartItems()}
+          </div>
+          <div className='col-11 m-auto d-flex justify-content-between align-items-center'>
+            <h2 className="cart-total">
+              {`Total: $${CalculateTotalCost(this.props)}.00`}
+            </h2>
+            <button
+              className={this.props.cart.length === 0 ? 'checkout-btn btn btn-primary d-none' : 'checkout-btn btn btn-primary' }
+              onClick={() => this.props.setView('checkout', {})}>
             Checkout
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     );
